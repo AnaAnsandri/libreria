ba3373a7b255ffeed92f66d769b541bd82d2986b
fedeyana777 <147662691+fedeyana777@users.noreply.github.com>
2024-04-03 00:21:14 -0300

    Update Clothing.java

--- curso/Clothing.java
+++ curso/Clothing.java
@@ -1,9 +1,63 @@
 public class Clothing {
-    String description;
-    Double price;
-    String size;
-    Clothing(){
-        size="M";
+    String descripcion;
+    Double Precio;
+    String talle = "S";
+    public final static double minPrecio = 8.0;
+    public final static double minTax = 0.5;
+
+
+    public Clothing(String descripcion, double Precio, String talle) {
+        this.descripcion = descripcion;
+        this.Precio = Precio;
+        this.talle = talle;
     }
-}
+
+    public Clothing(String descripcion, String talle) {
+        this.descripcion = desccripcion;
+        this.talle = talle;
+    }
+    public Clothing(double Precio) {
+        this.Precio = Precio;
+    }
+
+    public Clothing() {
+    }
+
+    public Clothing(String talle) {
+        this.talle = talle;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getalle() {
+        return talle;
+    }
+     public String getdescripcion() {
+        return descripcion;
+    }
+
+    public void setSize(String talle) {
+        this.talle = talle;
+    }
+
+    public void setdescripcion(String descricion, double Precio, String talle) {
+        this.descripcion = descripcion;
+        this.Precio = Precio;
+        this.talle = talle;
+    }
+    
+    public double Precio() {
+        return Precio + (Precio * minTax);
+    }
+
+    public void setPrecio(double Precio) {
+        if (Precio > minPrecio){
+            this.Precio = Precio;
+        }
+    }
+
+    }
+
 
